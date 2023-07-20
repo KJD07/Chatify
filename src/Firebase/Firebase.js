14cbd20131746e212b1b0872c33988b751f9d91f
KJD07 <100962576+KJD07@users.noreply.github.com>
2023-07-20 00:23:09 -0700

    Add files via upload

---
+++ src/Firebase/Firebase.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDEAn_WhvzsfxXt97vOXBh0Hb7J9jiGZhA",
+  authDomain: "webchat-39b0a.firebaseapp.com",
+  projectId: "webchat-39b0a",
+  storageBucket: "webchat-39b0a.appspot.com",
+  messagingSenderId: "913149934038",
+  appId: "1:913149934038:web:a8d8a6eef1aec4c72a0c77",
+  measurementId: "G-Q29HVZCXTJ"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+export { db, auth, provider, storage };
+export default firebaseApp;
